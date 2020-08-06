be3dc95641ac37ebf5383601525bc534f9cd3e94
DESKTOP-IADE8J9\lenov <lenovovs960@gmail.com>
2020-08-06 14:47:41 +0300

    ShieldPositionScriptsADD
    ASDSA

---
+++ Assets/Scripts/ShieldPosition.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldPosition : MonoBehaviour
+{
+    public Rigidbody playerRB;
+    private void FixedUpdate()
+    {
+        transform.position = playerRB.position;
+    }
+}
